520c6129812a23668f8401e720d1cc88397b76a9
Logan McAnsh <logan@mcan.sh>
2020-03-03 15:30:13 -0500

    chore: log how many links were updated
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- doeet-mapper-safari/doeet-mapper-safari/index.js
+++ doeet-mapper-safari/doeet-mapper-safari/index.js
@@ -1,23 +1,23 @@
 // remap all links to vscode:// to vscode-insiders://
-document.addEventListener("DOMContentLoaded", event => {
-    function updateLinks() {
-        // 1. get all links that start with `vscode:`
-        const codeLinks = Array.from(document.querySelectorAll('a[href^="vscode:"]'));
-        // 2. if there are none, do nothing
-        if (!codeLinks.length)
-            return;
-        codeLinks.forEach((link) => {
-            const url = new URL(link.href);
-            // 3. replace stable protocol with the insiders protocol
-            url.protocol = "vscode-insiders:";
-            link.href = url.href;
-        });
-    }
-    var observer = new MutationObserver(() => updateLinks());
-    observer.observe(document, {
-        attributes: false,
-        childList: true,
-        characterData: false,
-        subtree: true
+function updateLinks() {
+    // 1. get all links that start with `vscode:`
+    const codeLinks = Array.from(document.querySelectorAll('a[href^="vscode:"]'));
+    console.log(`Updating ${codeLinks.length} link${codeLinks.length !== 1 ? "s" : ""}`);
+    // 2. if there are none, do nothing
+    if (!codeLinks.length)
+        return;
+    codeLinks.forEach((link) => {
+        const url = new URL(link.href);
+        // 3. replace stable protocol with the insiders protocol
+        url.protocol = "vscode-insiders:";
+        link.href = url.href;
     });
+}
+updateLinks();
+var observer = new MutationObserver(() => updateLinks());
+observer.observe(document, {
+    attributes: false,
+    childList: true,
+    characterData: false,
+    subtree: true
 });
--- source/index.ts
+++ source/index.ts
@@ -1,27 +1,30 @@
 // remap all links to vscode:// to vscode-insiders://
 
-document.addEventListener("DOMContentLoaded", event => {
-  function updateLinks() {
-    // 1. get all links that start with `vscode:`
-    const codeLinks = Array.from(
-      document.querySelectorAll('a[href^="vscode:"]')
-    );
-    // 2. if there are none, do nothing
-    if (!codeLinks.length) return;
-    codeLinks.forEach((link: HTMLAnchorElement) => {
-      const url = new URL(link.href);
-      // 3. replace stable protocol with the insiders protocol
-      url.protocol = "vscode-insiders:";
-      link.href = url.href;
-    });
-  }
+function updateLinks() {
+  // 1. get all links that start with `vscode:`
+  const codeLinks = Array.from(document.querySelectorAll('a[href^="vscode:"]'));
 
-  var observer = new MutationObserver(() => updateLinks());
+  console.log(
+    `Updating ${codeLinks.length} link${codeLinks.length === 1 ? "" : "s"}`
+  );
 
-  observer.observe(document, {
-    attributes: false,
-    childList: true,
-    characterData: false,
-    subtree: true
+  // 2. if there are none, do nothing
+  if (!codeLinks.length) return;
+  codeLinks.forEach((link: HTMLAnchorElement) => {
+    const url = new URL(link.href);
+    // 3. replace stable protocol with the insiders protocol
+    url.protocol = "vscode-insiders:";
+    link.href = url.href;
   });
+}
+
+updateLinks();
+
+var observer = new MutationObserver(() => updateLinks());
+
+observer.observe(document, {
+  attributes: false,
+  childList: true,
+  characterData: false,
+  subtree: true
 });
