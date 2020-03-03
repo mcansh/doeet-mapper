65af7370f0f3fdc7f73d5d7c627184445d825b9f
Logan McAnsh <logan@mcan.sh>
2020-03-03 10:54:17 -0500

    chore: updates
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- doeet-mapper-safari/doeet-mapper-safari/index.js
+++ doeet-mapper-safari/doeet-mapper-safari/index.js
@@ -1,14 +1,23 @@
-function getSomeCandy() {
-    // 1. get all links that start with `vscode:`
-    const codeLinks = Array.from(document.querySelectorAll('a[href^="vscode:"]'));
-    // 2. if there are none, do nothing
-    if (!codeLinks.length)
-        return;
-    codeLinks.forEach((link) => {
-        const url = new URL(link.href);
-        // 3. replace stable protocol with the insiders protocol
-        url.protocol = 'vscode-insiders:';
-        link.href = url.href;
+// remap all links to vscode:// to vscode-insiders://
+document.addEventListener("DOMContentLoaded", event => {
+    function updateLinks() {
+        // 1. get all links that start with `vscode:`
+        const codeLinks = Array.from(document.querySelectorAll('a[href^="vscode:"]'));
+        // 2. if there are none, do nothing
+        if (!codeLinks.length)
+            return;
+        codeLinks.forEach((link) => {
+            const url = new URL(link.href);
+            // 3. replace stable protocol with the insiders protocol
+            url.protocol = "vscode-insiders:";
+            link.href = url.href;
+        });
+    }
+    var observer = new MutationObserver(() => updateLinks());
+    observer.observe(document, {
+        attributes: false,
+        childList: true,
+        characterData: false,
+        subtree: true
     });
-}
-getSomeCandy();
+});
--- source/index.ts
+++ source/index.ts
@@ -1,14 +1,27 @@
-function getSomeCandy() {
-  // 1. get all links that start with `vscode:`
-  const codeLinks = Array.from(document.querySelectorAll('a[href^="vscode:"]'));
-  // 2. if there are none, do nothing
-  if (!codeLinks.length) return;
-  codeLinks.forEach((link: HTMLAnchorElement) => {
-    const url = new URL(link.href);
-    // 3. replace stable protocol with the insiders protocol
-    url.protocol = 'vscode-insiders:';
-    link.href = url.href;
-  });
-}
+// remap all links to vscode:// to vscode-insiders://
 
-getSomeCandy();
+document.addEventListener("DOMContentLoaded", event => {
+  function updateLinks() {
+    // 1. get all links that start with `vscode:`
+    const codeLinks = Array.from(
+      document.querySelectorAll('a[href^="vscode:"]')
+    );
+    // 2. if there are none, do nothing
+    if (!codeLinks.length) return;
+    codeLinks.forEach((link: HTMLAnchorElement) => {
+      const url = new URL(link.href);
+      // 3. replace stable protocol with the insiders protocol
+      url.protocol = "vscode-insiders:";
+      link.href = url.href;
+    });
+  }
+
+  var observer = new MutationObserver(() => updateLinks());
+
+  observer.observe(document, {
+    attributes: false,
+    childList: true,
+    characterData: false,
+    subtree: true
+  });
+});
